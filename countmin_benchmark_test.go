77c45e92c6928f2c7fc580fd714cd45dad3d1312
Chavez <matthew@el-chavez.me>
2014-12-24 11:14:09 -0700

    Benchmark Add

--- countmin_benchmark_test.go
+++ countmin_benchmark_test.go
@@ -1,6 +1,9 @@
 package countmin
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Benchmark_New_1000(b *testing.B)  { benchNew(b, 1000) }
 func Benchmark_New_10000(b *testing.B) { benchNew(b, 10000) }
@@ -8,5 +11,15 @@ func Benchmark_New_10000(b *testing.B) { benchNew(b, 10000) }
 func benchNew(b *testing.B, total int) {
 	for i := 0; i < b.N; i++ {
 		New(total, total)
+	}
+}
+
+func Benchmark_Add_1000(b *testing.B) { benchAdd(b, 1000) }
+func benchAdd(b *testing.B, total int) {
+	cm := New(200, 200)
+	b.ResetTimer()
+	var i int64
+	for i = 0; i < int64(b.N); i++ {
+		cm.Add([]byte(fmt.Sprintf("http://domain%d.com/page%d", i, i)), i)
 	}
 }
