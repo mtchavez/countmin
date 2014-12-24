6d3f9162b79b28cc1f9968b3f9673021659acd29
Chavez <matthew@el-chavez.me>
2014-12-24 10:28:54 -0700

    Add example

--- example_test.go
+++ example_test.go
@@ -1,13 +1,11 @@
-package main
+package countmin
 
 import (
 	"fmt"
-
-	"github.com/mtchavez/countmin/countmin"
 )
 
-func main() {
-	cm := countmin.New(10, 100000000)
+func ExampleAdd() {
+	cm := New(10, 100000000)
 	for _, i := range []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1} {
 		cm.Add([]byte(fmt.Sprintf("%d", i)), int64(i))
 	}
@@ -17,4 +15,11 @@ func main() {
 	fmt.Println("Size: ", cm.Size())
 	fmt.Println("Err: ", cm.RelativeError())
 	fmt.Println("Confidence: ", cm.Confidence())
+	// Output:
+	// Estimate of 1 is 3
+	// Estimate of 3 is 6
+	// Estimate of 9 is 18
+	// Size:  91
+	// Err:  2e-08
+	// Confidence:  0.9990234375
 }
