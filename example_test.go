7ec15f5bfd79e28bb2d6a5c1ba2be8feb5aba5c9
Chavez <matthew@el-chavez.me>
2016-04-12 17:25:20 -0700

    Add Merge() example doc test

--- example_test.go
+++ example_test.go
@@ -23,3 +23,29 @@ func ExampleCountMin_Add() {
 	// Err:  2e-08
 	// Confidence:  0.9990234375
 }
+
+func ExampleCountMin_Merge() {
+	cm1 := New(10, 100000000)
+	for _, i := range []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1} {
+		cm1.Add([]byte(fmt.Sprintf("%d", i)), int64(i))
+	}
+
+	cm2 := New(10, 100000000)
+	for _, i := range []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1} {
+		cm2.Add([]byte(fmt.Sprintf("%d", i)), int64(i))
+	}
+	merged, _ := Merge(cm1, cm2)
+	fmt.Printf("Estimate of %d is %d\n", 1, merged.Count([]byte("1")))
+	fmt.Printf("Estimate of %d is %d\n", 3, merged.Count([]byte("3")))
+	fmt.Printf("Estimate of %d is %d\n", 9, merged.Count([]byte("9")))
+	fmt.Println("Size: ", merged.Size())
+	fmt.Println("Err: ", merged.RelativeError())
+	fmt.Println("Confidence: ", merged.Confidence())
+	// Output:
+	// Estimate of 1 is 6
+	// Estimate of 3 is 12
+	// Estimate of 9 is 36
+	// Size:  182
+	// Err:  2e-08
+	// Confidence:  0.9990234375
+}
